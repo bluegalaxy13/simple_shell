95ea12f19f83bdf66e45ba9dbe65d1520c50b34a
bluegalaxy13 <ricardosassofficial@gmail.com>
2023-04-17 14:51:16 +0200

    Added .h file and shell initiation file

---
+++ shell.h
@@ -0,0 +1,23 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+/*==================================================*/
+/*============     Shell_Init       ==============*/
+/*==================================================*/
+
+int main(int ac, char **av, char **env);
+void prompt(void);
+void handle(int signals);
+void _EOF(char *buffer);
+void shell_exit(char **command);
+
+/*============ END      ==============*/
+
+#endif /* _SHELL_H_ */
