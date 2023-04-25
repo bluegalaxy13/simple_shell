08b54d1c677e791f417fc8f7fca5b135f5db5274
PhilBoa <philicbrefo98@gmail.com>
2023-04-25 14:38:32 -0700

    My updated execute.c file

--- execute.c
+++ execute.c
@@ -8,6 +8,7 @@
  * @cicles: Number of executed cicles.
  * Return: Nothing.
  */
+
 void execute(char **command, char *name, char **env, int cicles)
 {
 	char **pathways = NULL, *full_path = NULL;
@@ -16,39 +17,36 @@ void execute(char **command, char *name, char **env, int cicles)
 
 	if (_strcmp(command[0], "env") != 0)
 	{
-        print_env(env);
-    }
-    
+	print_env(env);
+	}
 	if (stat(command[0], &st) == 0)
 	{
-		if (execve(command[0], command, env) < 0)
-		{
-			perror(name);
-			free_exit(command);
-		}
+	if (execve(command[0], command, env) < 0)
+	{
+	perror(name);
+	free_exit(command);
+	}
 	}
 	else
 	{
-		pathways = _getPATH(env);
-
-		while (pathways[i])
-		{
-			full_path = _strcat(pathways[i], command[0]);
-			i++;
-
-			if (stat(full_path, &st) == 0)
-			{
-				if (execve(full_path, command, env) < 0)
-				{
-					perror(name);
-					free_dp(pathways);
-					free_exit(command);
-				}
-				return;
-			}
-		}
-		msgerror(name, cicles, command);
-		free_dp(pathways);
+	pathways = _getPATH(env);
+	while (pathways[i])
+	{
+	full_path = _strcat(pathways[i], command[0]);
+	i++;
+	if (stat(full_path, &st) == 0)
+	{
+	if (execve(full_path, command, env) < 0)
+	{
+	perror(name);
+	free_dp(pathways);
+	free_exit(command);
+	}
+	return;
+	}
+	}
+	msgerror(name, cicles, command);
+	free_dp(pathways);
 	}
 }
 
@@ -57,26 +55,28 @@ void execute(char **command, char *name, char **env, int cicles)
  * @env: The pointer to enviromental variables.
  * Return: Nothing.
  */
+
 void print_env(char **env)
 {
 	size_t i = 0, len = 0;
 
 	while (env[i])
 	{
-		len = _strlen(env[i]);
-		write(STDOUT_FILENO, env[i], len);
-		write(STDOUT_FILENO, "\n", 1);
-		i++;
+	len = _strlen(env[i]);
+	write(STDOUT_FILENO, env[i], len);
+	write(STDOUT_FILENO, "\n", 1);
+	i++;
 	}
 }
-
 
 /**
  * _getPATH - A function to gets the full value from
  * enviromental variable PATH.
  * @env: The pointer to enviromental variables.
  * Return: All tokenized pathways for commands.
+ *
  */
+
 char **_getPATH(char **env)
 {
 	char *pathvalue = NULL, **pathways = NULL;
@@ -85,14 +85,14 @@ char **_getPATH(char **env)
 	pathvalue = strtok(env[i], "=");
 	while (env[i])
 	{
-		if (_strcmp(pathvalue, "PATH"))
-		{
-			pathvalue = strtok(NULL, "\n");
-			pathways = tokening(pathvalue, ":");
-			return (pathways);
-		}
-		i++;
-		pathvalue = strtok(env[i], "=");
+	if (_strcmp(pathvalue, "PATH"))
+	{
+	pathvalue = strtok(NULL, "\n");
+	pathways = tokening(pathvalue, ":");
+	return (pathways);
+	}
+	i++;
+	pathvalue = strtok(env[i], "=");
 	}
 	return (NULL);
 }
@@ -104,6 +104,7 @@ char **_getPATH(char **env)
  * @command: The pointer to tokenized command.
  * Return: Nothing.
  */
+
 void msgerror(char *name, int cicles, char **command)
 {
 	char c;
