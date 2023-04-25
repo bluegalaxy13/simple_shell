2d919548f50825371cc471ca6446faeafa486e96
PhilBoa <philicbrefo98@gmail.com>
2023-04-25 15:24:14 -0700

    Entails tokening functions

--- tokening.c
+++ tokening.c
@@ -6,6 +6,7 @@
  * @buffer: The pointer to input string.
  * Return: A string with full command.
  */
+
 char **tokening(char *buffer, const char *s)
 {
 	char *token = NULL, **commands = NULL;
@@ -14,32 +15,30 @@ char **tokening(char *buffer, const char *s)
 
 	if (buffer == NULL)
 	{
-		return (NULL);
-	}	
-
+	return (NULL);
+	}
 	bufsize = _strlen(buffer);
 	commands = malloc((bufsize + 1) * sizeof(char *));
 	if (commands == NULL)
 	{
-		perror("Unable to allocate buffer");
-		free(buffer);
-		free_dp(commands);
-		exit(EXIT_FAILURE);
+	perror("Unable to allocate buffer");
+	free(buffer);
+	free_dp(commands);
+	exit(EXIT_FAILURE);
 	}
-
 	token = strtok(buffer, s);
 	while (token != NULL)
 	{
-		commands[i] = malloc(_strlen(token) + 1);
-		if (commands[i] == NULL)
-		{
-			perror("Unable to allocate buffer");
-			free_dp(commands);
-			return (NULL);
-		}
-		_strcpy(commands[i], token);
-		token = strtok(NULL, s);
-		i++;
+	commands[i] = malloc(_strlen(token) + 1);
+	if (commands[i] == NULL)
+	{
+	perror("Unable to allocate buffer");
+	free_dp(commands);
+	return (NULL);
+	}
+	_strcpy(commands[i], token);
+	token = strtok(NULL, s);
+	i++;
 	}
 	commands[i] = NULL;
 	return (commands);
